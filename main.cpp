ebb3d64153fd62dce4c3801c1315913f3384d12d
panhn <86941316+panhn@users.noreply.github.com>
2024-08-09 15:27:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,22 +6,14 @@ using namespace std;
 
 int main()
 {
-	input = imread("D:/test_img/TP (476).JPG");
+	input = imread("filepath");
 
 	namedWindow(windowname, 0);
 	resizeWindow(windowname, 540, 900);
 	imshow(windowname, input);
-	
-	/*auto starttime = chrono::system_clock::now();
 
-	cv::Mat out = ColorTemperature1(src, temp);
-
-	auto diff = chrono::duration_cast<std::chrono::milliseconds>(chrono::system_clock::now() - starttime).count();
-	cout << "所耗时间为：" << diff << "ms" << endl;
-	cv::imwrite("out1.jpg", out);*/
-
-
-	//createTrackbar("tempera", windowname, &temperature, 2 * maxValue, onChangeTempera);
+	//choose one or more functions to display
+	createTrackbar("tempera", windowname, &temperature, 2 * maxValue, onChangeTempera);
 	//createTrackbar("tempera1", windowname, &temperature1, 2 * maxValue, onChangeTempera1);
 	//createTrackbar("shadow", windowname, &light, 2 * maxValue, onChangeShadow);
 	//createTrackbar("saturation", windowname, &sat, 2 * maxValue, onChangeSaturation);
