29edc00fdfb72ec49e2b7e7794c3d8b1b011f3a4
panhn <86941316+panhn@users.noreply.github.com>
2024-08-29 17:44:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream> 
 #include "func.hpp"
+#include "ColorStyleTransfer.hpp"
 
 using namespace cv;
 using namespace std;
@@ -9,17 +10,21 @@ int main()
 	input = imread("filepath");
 
 	namedWindow(windowname, 0);
-	resizeWindow(windowname, 540, 900);
+	//resizeWindow(windowname, 540, 900);
 	imshow(windowname, input);
 
 	//choose one or more functions to display
-	createTrackbar("tempera", windowname, &temperature, 2 * maxValue, onChangeTempera);
+	//createTrackbar("tempera", windowname, &temperature, 2 * maxValue, onChangeTempera);
 	//createTrackbar("tempera1", windowname, &temperature1, 2 * maxValue, onChangeTempera1);
 	//createTrackbar("shadow", windowname, &light, 2 * maxValue, onChangeShadow);
 	//createTrackbar("saturation", windowname, &sat, 2 * maxValue, onChangeSaturation);
 	//createTrackbar("definition", windowname, &def, 2 * maxValue, onChangeDefinition);
 	//createTrackbar("exposure", windowname, &tone, 2 * maxValue, onChangeExposure);
 	//createTrackbar("vividness", windowname, &vividness, 2 * maxValue, onChangeVividness);
+	createTrackbar("lumdegree", windowname, &lumdegree, maxValue, onRemoveNoise);
+	createTrackbar("lumclear", windowname, &lumclear, maxValue, onRemoveNoise);
+	createTrackbar("colordegree", windowname, &colordegree, maxValue, onRemoveNoise);
+	createTrackbar("colorclear", windowname, &colorclear, maxValue, onRemoveNoise);
 	
 	waitKey();
 	return 0;
